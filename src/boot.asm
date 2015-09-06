66af5cf4be70697047629179f20c4b22453412bf
kernal88 <8332659@sina.com>
2015-09-06 21:06:50 +0800

    show msg

--- src/boot.asm
+++ src/boot.asm
@@ -8,8 +8,20 @@ msgstr:
 start:
 	cli
 	
-	
-	
+	mov ax,0x00
+    mov es,ax
+    mov bp,msgstr
+    mov ah,0x13
+    mov al,0x01
+    mov bh,0x00
+    mov bl,0x01
+    mov cx,0x000b
+    int 10
+
+loop:
+    halt
+    jmp loop
+
 	times 510-($-$$) db 0
 	db 0x55, 0xaa
 	

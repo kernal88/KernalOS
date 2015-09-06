9785a26cd50f9fc1ee19b5970ca9747021ca21e2
kernal88 <61250@LP-61250.casco.signal>
2015-09-06 15:53:54 +0800

    update boot.asm

--- src/boot.asm
+++ src/boot.asm
@@ -8,6 +8,16 @@ msgstr:
 start:
 	cli
 	
+	;show Hello World
+	mov ax,0x0000
+	mov es,ax
+	mov bp,msgstr
+	mov ah,0x13
+	mov al,0x01
+	mov bh,0x00
+	mov bl,0x07
+	mov cx,0x000c
+	mov dx,0x0a0a
 	
 	
 	times 510-($-$$) db 0

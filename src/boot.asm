d5082cee59a31c0efc2ecb53a7dc67480ab5e603
kernal88 <61250@LP-61250.casco.signal>
2015-09-07 18:54:11 +0800

    clear screen && print screen

--- src/boot.asm
+++ src/boot.asm
@@ -8,7 +8,14 @@ msgstr:
 start:
 	cli
 	
-<<<<<<< HEAD
+	;clear screen
+	mov ah,0x06
+	mov al,0x00
+	mov bh,0x07
+	mov cx,0x0000
+	mov dh,24
+	mov dl,79
+	int 0x10
 	;show Hello World
 	mov ax,0x0000
 	mov es,ax
@@ -18,25 +25,13 @@ start:
 	mov bh,0x00
 	mov bl,0x07
 	mov cx,0x000c
-	mov dx,0x0a0a
-	
-	
-=======
-	mov ax,0x00
-    mov es,ax
-    mov bp,msgstr
-    mov ah,0x13
-    mov al,0x01
-    mov bh,0x00
-    mov bl,0x01
-    mov cx,0x000b
-    int 10
+	mov dx,0x0000
+    int 0x10
 
 loop:
-    halt
+    hlt
     jmp loop
 
->>>>>>> origin/master
 	times 510-($-$$) db 0
 	db 0x55, 0xaa
 	
